5346b198dd41dc8c1a318f6d18067276d55f81f5
Matthew Choi <choix709@umn.edu>
2021-08-17 16:10:17 -0500

    created flask react template

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+# db = SQLAlchemy(app)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
