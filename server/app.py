8ee10ba4bf71895b7aa7420279c2330439276ab3
Matthew Choi <choix709@umn.edu>
2021-08-17 17:37:54 -0500

    added spotipy authentication

--- server/app.py
+++ server/app.py
@@ -1,11 +1,54 @@
-from flask import Flask
+import os
+
+from flask import Flask,  request
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from markupsafe import escape
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 CORS(app)
 # db = SQLAlchemy(app)
 
+# to start app
+# set FLASK_APP=app (or set FLASK_APP since files is called app.py)
+# flask run
+
+# enable dev mode be like
+# set FLASK_ENV=development
+# flask run
+
+
+client = os.getenv('client_id')
+secret = os.getenv('client_secret')
+uri = os.getenv('redirect_uri')
+scope = 'user-library-read'
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client,
+                                               client_secret=secret,
+                                               redirect_uri=uri,
+                                               scope=scope))
+                                        
+results = sp.current_user_saved_tracks()
+for idx, item in enumerate(results['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return f"client: {client}, secret: {secret}"
+
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+    
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if request.method == 'POST':
+#         return "<p>Hello, World!</p>"
+#     else:
+#         return "<p>Hello, World!</p>"
